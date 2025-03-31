3fdd403f27ec8531ffd3d577f3ad15aef155af30
Bhawani Singh <bs190520@gmail.com>
2025-04-01 01:13:24 +0530

    dump and Homepage

---
+++ rimxui/src/pages/Dump.tsx
@@ -0,0 +1,11 @@
+import Buttons from "../components/showcase/Buttons";
+
+const Dump = () => {
+  return (
+    <div className="flex justify-center items-center text-red-700 bg-slate-600 min-w-[80%] ">
+      <Buttons />
+    </div>
+  );
+};
+
+export default Dump;
