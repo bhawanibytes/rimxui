8e2765b0c0a56806404a606944818a5b6d63b187
kartik-dhawan-dev <kartikdhawan.dev@gmail.com>
2025-04-04 12:54:06 +0530

    feat(list): Add List Component

---
+++ rimxui/src/constants/components/list/list.styles.constants.ts
@@ -0,0 +1,21 @@
+import { ListSizes, ListSpacings, ListVariants } from "../../../types";
+
+export const LIST_VARIANTS_STYLES: Record<ListVariants, string> = {
+  default: "text-gray-800 dark:text-gray-100",
+  primary: "text-blue-700 dark:text-blue-300",
+  secondary: "text-purple-700 dark:text-purple-300",
+  outline:
+    "text-gray-800 dark:text-gray-100 border border-gray-200 dark:border-gray-700 rounded-md",
+};
+
+export const LIST_SIZES_STYLES: Record<ListSizes, string> = {
+  sm: "text-sm",
+  md: "text-base",
+  lg: "text-lg",
+};
+
+export const LIST_SPACING_STYLES: Record<ListSpacings, string> = {
+  small: "space-y-1",
+  medium: "space-y-2",
+  large: "space-y-4",
+};
