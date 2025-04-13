27446ea23b47a3728b7d738832ebeedee864e429
Md Arshad <mdarshd246@gmail.com>
2025-04-13 16:47:16 +0530

    command some components

--- rimxui/src/components/showcase/ShowcaseLayout.tsx
+++ rimxui/src/components/showcase/ShowcaseLayout.tsx
@@ -24,9 +24,9 @@ const navigation = [
   { name: "Countdown", path: ROUTES.counterShowcase },
   { name: "Darkmode", path: ROUTES.darkModeShowcase },
   { name: "ChatNotification", path: ROUTES.notificationShowcase },
-  { name: "Label", path: ROUTES.labelShowcase },
-  { name: "Input", path: ROUTES.inputShowcase },
-  { name: "textarea", path: ROUTES.textAreaShowcase },
+  // { name: "Label", path: ROUTES.labelShowcase },
+  // { name: "Input", path: ROUTES.inputShowcase },
+  // { name: "textarea", path: ROUTES.textAreaShowcase },
 ];
 
 export const ShowcaseLayout: React.FC = () => {
