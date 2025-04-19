c7768a3c1e5b79052c8b8ccde18a22711aee5583
Bhawani Singh <bs190520@gmail.com>
2025-04-19 06:04:38 +0530

    FAQ Wrapper done

--- rimxui/src/components/showcase/AccordionPage.tsx
+++ rimxui/src/components/showcase/AccordionPage.tsx
@@ -1,4 +1,5 @@
 import Accordion from "../Acoordion/accordion";
+import PreviewWrapper from "../PreviewWrapper";
 
 const faqItems = [
   {
@@ -27,13 +28,33 @@ export default function AccordionPage() {
   return (
     <div className="min-h-screen px-4 py-8 bg-white dark:bg-neutral-900 text-gray-800 dark:text-gray-200 transition-colors duration-300">
       <div className="max-w-2xl mx-auto">
-        <h2 className="text-3xl font-bold mb-6 text-center">FAQs</h2>
-        <Accordion
-          items={faqItems}
-          containerClass="bg-gray-50 dark:bg-zinc-800 p-4 rounded-xl border dark:border-zinc-700"
-          itemClass="mb-4 shadow-md dark:shadow-none border border-gray-200 dark:border-zinc-700 rounded-lg overflow-hidden"
-          titleClass="text-lg font-medium p-4 cursor-pointer bg-white dark:bg-zinc-800 hover:bg-gray-100 dark:hover:bg-zinc-700 transition-all duration-300 text-gray-800 dark:text-gray-100"
-          contentClass="p-4 text-sm text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-zinc-900"
+        <PreviewWrapper
+          label="FAQ"
+          variant="centered"
+          preview={
+            <>
+              <Accordion
+                items={faqItems}
+                containerClass="bg-gray-50 dark:bg-zinc-800 p-4 rounded-xl border dark:border-zinc-700"
+                itemClass="mb-4 shadow-md dark:shadow-none border border-gray-200 dark:border-zinc-700 rounded-lg overflow-hidden"
+                titleClass="text-lg font-medium p-4 cursor-pointer bg-white dark:bg-zinc-800 hover:bg-gray-100 dark:hover:bg-zinc-700 transition-all duration-300 text-gray-800 dark:text-gray-100"
+                contentClass="p-4 text-sm text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-zinc-900"
+              />
+            </>
+          }
+          code={`           
+            import {Accordion} from "@/components/ui/accordion";
+export function AccordionPage() {
+return (
+<Accordion
+items={faqItems}
+containerClass="bg-gray-50 dark:bg-zinc-800 p-4 rounded-xl border dark:border-zinc-700"
+itemClass="mb-4 shadow-md dark:shadow-none border border-gray-200 dark:border-zinc-700 rounded-lg overflow-hidden"
+titleClass="text-lg font-medium p-4 cursor-pointer bg-white dark:bg-zinc-800 hover:bg-gray-100 dark:hover:bg-zinc-700 transition-all duration-300 text-gray-800 dark:text-gray-100"
+contentClass="p-4 text-sm text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-zinc-900"
+/>
+)
+          `}
         />
       </div>
     </div>
