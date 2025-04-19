ad8d63cdcab068f54cc00a0387a027e76a900533
Bhawani Singh <bs190520@gmail.com>
2025-04-19 19:31:59 +0530

    previewWrappers in CLITabs

--- rimxui/src/components/showcase/CliTabPage.tsx
+++ rimxui/src/components/showcase/CliTabPage.tsx
@@ -1,20 +1,25 @@
-import { CliTabs } from "../CliTabs/CliTab"
+import { CliTabs } from "../CliTabs/CliTab";
+import PreviewWrapper from "../PreviewWrapper";
 
 const CliTabPage = () => {
   return (
     <div>
       <section id="#installation">
-          <h1 className="text-h4 mb-6">Installation</h1>
-          <CliTabs
-            commands={{
-              pnpm: "pnpm dlx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
-              npm: "npx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
-              bun: "bunx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
-              yarn: "yarn dlx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
-            }}
-          />
-        </section>
+        <h1 className="text-h4 mb-6">Installation</h1>
         <CliTabs
+          commands={{
+            pnpm: "pnpm dlx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
+            npm: "npx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
+            bun: "bunx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
+            yarn: "yarn dlx shadcn@latest add http://rimxui.vercel.app/registry/clitab.json",
+          }}
+        />
+      </section>
+      <PreviewWrapper
+        label="Cli Tabs"
+        variant="centered"
+        preview={
+          <CliTabs
             commands={{
               pnpm: "pnpm's example command",
               npm: "npx's example command",
@@ -22,8 +27,26 @@ const CliTabPage = () => {
               yarn: "yarn's example command",
             }}
           />
+        }
+        code={`
+import { CliTabs } from "@/components/ui/clitabs"
+  
+function clitab () {
+return (
+  <CliTabs
+    commands={{
+      pnpm: "pnpm's example command",
+      npm: "npx's example command",
+      bun: "bunx's example command",
+      yarn: "yarn's example command",
+    }}
+  />
+    )
+  }
+`}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default CliTabPage
+export default CliTabPage;
